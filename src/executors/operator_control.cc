f59d2c0e55dd0451d071def747ebcb34a2cfb63e
Tommy Persson <tommy.persson@liu.se>
2015-11-17 10:14:56 +0100

    Adding OperatorControl executor

---
+++ src/executors/operator_control.cc
@@ -0,0 +1,100 @@
+#include "operator_control.h"
+
+#include <iostream>
+
+#include "executil.h"
+
+extern ros::NodeHandle * global_nh;
+extern ros::Publisher * global_confirm_pub;
+
+extern std::map<std::string, boost::thread *> threadmap;
+
+using namespace std;
+
+
+Exec::OperatorControl::OperatorControl (std::string ns, int id) : Executor (ns, id), 
+								  enough_requested(false) {
+
+  add_resource_to_lock("fly");
+
+  lrs_msgs_tst::TSTExecInfo einfo;
+  einfo.can_be_aborted = true;
+  einfo.can_be_enoughed = true;
+  set_exec_info(ns, id, einfo);
+}
+
+bool Exec::OperatorControl::prepare () {
+  bool res = true;
+  ROS_INFO ("Exec::OperatorControl::prepare");
+
+  if (res) {
+    set_active_flag (node_ns, node_id, true);
+  }
+
+  return res;
+}
+
+
+void Exec::OperatorControl::start () {
+  ROS_INFO ("Exec::OperatorControl::start: %s - %d", node_ns.c_str(), node_id);
+
+  try {
+
+    if (!do_before_work ()) {
+      return;
+    }
+
+    ROS_INFO ("Exec::OperatorControl");
+
+    //
+    // Replace the sleep with useful work.
+    //
+
+    boost::this_thread::interruption_point();
+    for (int i=0; i<10000; i++) {
+      usleep(1000);
+      boost::this_thread::interruption_point();
+      if (enough_requested) {
+	break;
+      }
+    }
+
+    //
+    // When we reach this point the node execution whould be finished.
+    //
+
+    ROS_INFO ("Exec::OperatorControl: FINISHED");
+
+    wait_for_postwork_conditions ();
+  }
+  catch (boost::thread_interrupted) {
+    abort_fail ("OperatorControl ABORTED");
+    return;
+  }
+}
+
+bool Exec::OperatorControl::abort () {
+  bool res = false;
+  ROS_ERROR("Exec::OperatorControl::abort");
+  ostringstream os;
+  os << node_ns << "-" << node_id;
+  if (threadmap.find (os.str()) != threadmap.end()) {
+    ROS_ERROR("EXECUTOR EXISTS: Sending interrupt to running thread");
+    threadmap[os.str()]->interrupt();
+
+    // Platform specific things to to
+
+    return true;
+  } else {
+    ROS_ERROR ("Executor does not exist: %s", os.str().c_str());
+    return false;
+  }
+  return res;
+}
+
+bool Exec::OperatorControl::enough_execution () {
+  bool res = true;
+  ROS_ERROR ("Exec::OperatorControl::enough_execution");
+  enough_requested = true;
+  return res;
+}
---
+++ src/executors/operator_control.h
@@ -0,0 +1,32 @@
+// This may look like C code, but it is really -*- C++ -*-
+
+#ifndef _OPERATOR_CONTROL_H
+#define _OPERATOR_CONTROL_H
+
+#include "executor.h"
+
+namespace Exec {
+
+  class OperatorControl : public virtual Executor {
+  private:
+    bool enough_requested;
+    bool continue_requested;
+    double x;
+    double y;
+    double z;
+    double speed;
+
+  public:
+    OperatorControl (std::string ns, int id);
+    virtual ~OperatorControl () {};
+
+    bool prepare ();
+    void start ();
+    bool abort ();
+
+    virtual bool enough_execution ();
+  };
+
+};
+
+#endif
