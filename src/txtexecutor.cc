d7b1096830b5c1a1883f929c8c46f9838e966cc1
Tommy Persson <tommy.persson@liu.se>
2016-09-21 11:27:40 +0200

    Lock resource check during delegation implemented.

--- src/txtexecutor.cc
+++ src/txtexecutor.cc
@@ -194,6 +194,7 @@ int main(int argc, char **argv) {
   services.push_back (n.advertiseService(prefix + "destroy_executor", destroy_executor));
   services.push_back (n.advertiseService(prefix + "create_executor", create_executor));
   services.push_back (n.advertiseService(prefix + "executor_check", executor_check));
+  services.push_back (n.advertiseService(prefix + "executor_lock_resources_check", executor_lock_resources_check));
   services.push_back (n.advertiseService(prefix + "executor_continue", executor_continue));
   services.push_back (n.advertiseService(prefix + "executor_expand", executor_expand));
   services.push_back (n.advertiseService(prefix + "executor_is_delegation_expandable", executor_is_delegation_expandable));
